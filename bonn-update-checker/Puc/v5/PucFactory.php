998c0f99714e648ee097e42553f6cd1cba446e91
bonn <bonnbonito@gmail.com>
2023-04-15 13:59:51 +0800

    init

---
+++ bonn-update-checker/Puc/v5/PucFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bonn\PluginUpdateChecker\v5;
+
+if ( !class_exists(PucFactory::class, false) ):
+
+	class PucFactory extends \Bonn\PluginUpdateChecker\v5p0\PucFactory {
+	}
+
+endif;
---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Check updater
+ */
+require plugin_dir_path( __FILE__ ) . 'bonn-update-checker/plugin-update-checker.php';
+
+use Bonn\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/bonnbonito/phlash-govcentre/',
+	__FILE__,
+	'phlash-govcentre'
+);
+
+// Set the branch that contains the stable release.
+$myUpdateChecker->setBranch( 'master' );
+
+/**
+ * Enqueue child styles.
+ */
+function child_enqueue_styles() {
+	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
+}
+
+// add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
+
+/**
+ * Add custom functions here
+ */
